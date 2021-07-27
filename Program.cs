f59ac8430f2403948b42d4f05bf8b0002bd86da7
Drew Wilson <jankyy93@gmail.com>
2021-07-27 13:56:54 -0400

    Created two lists

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeckCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var deckRanks = new List<string>() { };
+            deckRanks.Add("Ace");
+            deckRanks.Add("2");
+            deckRanks.Add("3");
+            deckRanks.Add("4");
+            deckRanks.Add("5");
+            deckRanks.Add("6");
+            deckRanks.Add("7");
+            deckRanks.Add("8");
+            deckRanks.Add("9");
+            deckRanks.Add("10");
+            deckRanks.Add("Jack");
+            deckRanks.Add("Queen");
+            deckRanks.Add("King");
+            deckRanks.ForEach(Console.WriteLine);
+
+            var deckSuits = new List<string>() { };
+            deckSuits.Add("of Clubs");
+            deckSuits.Add("of Diamonds");
+            deckSuits.Add("of Hearts");
+            deckSuits.Add("of Spades");
+            deckSuits.ForEach(Console.WriteLine);
+        }
     }
-  }
 }
