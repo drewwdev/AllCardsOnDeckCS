594880e46f876642578987d703665c4bf4ad9efb
Drew Wilson <jankyy93@gmail.com>
2021-07-27 19:42:25 -0400

    Done with explorer mode

--- Program.cs
+++ Program.cs
@@ -31,13 +31,35 @@ namespace AllCardsOnDeckCS
             var deck = new List<string>() { };
 
             Console.WriteLine();
-            foreach (string card in deckRanks)
+            foreach (string rank in deckRanks)
             {
-                foreach (string aCard in deckSuits)
+                foreach (string suit in deckSuits)
                 {
-                    Console.WriteLine($"{card}{aCard}");
+                    deck.Add($"{rank}{suit}");
                 }
             }
+
+            var numberOfCards = deck.Count;
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 1; rightIndex--)
+            {
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+                var leftCard = deck[leftIndex];
+                var rightCard = deck[rightIndex];
+                deck[rightIndex] = leftCard;
+                deck[leftIndex] = rightCard;
+            }
+
+            var firstCard = deck[51];
+            var secondCard = deck[50];
+            Console.WriteLine($"Your first card is {firstCard} and your second card is {secondCard}");
+
+            var playerHand = new List<string>() { };
+            playerHand.Add(firstCard);
+            deck.Remove(firstCard);
+            playerHand.Add(secondCard);
+            deck.Remove(secondCard);
+            Console.WriteLine($"Player hand now holds {playerHand[0]} and {playerHand[1]} and the deck now contains {deck.Count} cards");
         }
     }
 }
