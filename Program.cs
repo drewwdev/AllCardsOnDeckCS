6e60eba65926812a0dd2fced152c574f272189d0
Drew Wilson <jankyy93@gmail.com>
2021-07-27 16:46:54 -0400

    Got my for loops working to create deck of cards

--- Program.cs
+++ Program.cs
@@ -8,27 +8,36 @@ namespace AllCardsOnDeckCS
         static void Main(string[] args)
         {
             var deckRanks = new List<string>() { };
-            deckRanks.Add("Ace");
-            deckRanks.Add("2");
-            deckRanks.Add("3");
-            deckRanks.Add("4");
-            deckRanks.Add("5");
-            deckRanks.Add("6");
-            deckRanks.Add("7");
-            deckRanks.Add("8");
-            deckRanks.Add("9");
-            deckRanks.Add("10");
-            deckRanks.Add("Jack");
-            deckRanks.Add("Queen");
-            deckRanks.Add("King");
-            deckRanks.ForEach(Console.WriteLine);
+            deckRanks.Add("Ace ");
+            deckRanks.Add("2 ");
+            deckRanks.Add("3 ");
+            deckRanks.Add("4 ");
+            deckRanks.Add("5 ");
+            deckRanks.Add("6 ");
+            deckRanks.Add("7 ");
+            deckRanks.Add("8 ");
+            deckRanks.Add("9 ");
+            deckRanks.Add("10 ");
+            deckRanks.Add("Jack ");
+            deckRanks.Add("Queen ");
+            deckRanks.Add("King ");
 
             var deckSuits = new List<string>() { };
             deckSuits.Add("of Clubs");
             deckSuits.Add("of Diamonds");
             deckSuits.Add("of Hearts");
             deckSuits.Add("of Spades");
-            deckSuits.ForEach(Console.WriteLine);
+
+            var deck = new List<string>() { };
+
+            Console.WriteLine();
+            foreach (string card in deckRanks)
+            {
+                foreach (string aCard in deckSuits)
+                {
+                    Console.WriteLine($"{card}{aCard}");
+                }
+            }
         }
     }
 }
